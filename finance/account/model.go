1e6bb8eb9950413edbdb2b1b7b71c370ae02d1ae
ametsuramet <ametsuramet@gmail.com>
2025-01-05 11:11:36 +0700

    feat: update AccountModel to allow nullable CompanyID and add sample chart of accounts

--- finance/account/model.go
+++ finance/account/model.go
@@ -31,29 +31,29 @@ const (
 // AccountModel adalah model database untuk account
 type AccountModel struct {
 	utils.BaseModel
-	Name                  string               `json:"name" bson:"name"`
-	Code                  string               `json:"code" bson:"code"`
-	Color                 string               `json:"color" bson:"color"`
-	Description           string               `json:"description" bson:"description"`
-	IsDeletable           bool                 `json:"is_deletable" bson:"is_deletable"`
-	IsReport              bool                 `json:"is_report" bson:"is_report" gorm:"-"`
-	IsAccountReport       bool                 `json:"is_account_report" bson:"is_account_report" gorm:"-"`
-	IsCashflowReport      bool                 `json:"is_cashflow_report" bson:"is_cashflow_report" gorm:"-"`
-	IsPDF                 bool                 `json:"is_pdf" bson:"is_pdf" gorm:"-"`
-	Type                  AccountType          `json:"type" bson:"type"`
-	Category              string               `json:"category" bson:"category"`
-	CashflowGroup         string               `json:"cashflow_group" bson:"cashflow_group"`
-	CashflowSubGroup      string               `json:"cashflow_subgroup" bson:"cashflow_group"`
-	IsTax                 bool                 `json:"is_tax" bson:"is_tax" gorm:"default:false"`
-	TypeLabel             string               `gorm:"-" json:"type_label"`
-	CashflowGroupLabel    string               `gorm:"-" json:"cashflow_group_label"`
-	CashflowSubGroupLabel string               `gorm:"-" json:"cashflow_subgroup_label" `
-	CompanyID             string               `json:"company_id" gorm:"not null"`
-	Company               company.CompanyModel `gorm:"foreignKey:CompanyID"`
-	TransactionCount      int64                `gorm:"-"`
-	Balance               float64              `gorm:"-"`
-	BalanceBefore         float64              `gorm:"-"`
-	HasOpeningBalance     bool                 `gorm:"-"`
+	Name                  string                `json:"name" bson:"name"`
+	Code                  string                `json:"code" bson:"code"`
+	Color                 string                `json:"color" bson:"color"`
+	Description           string                `json:"description" bson:"description"`
+	IsDeletable           bool                  `json:"is_deletable" bson:"is_deletable"`
+	IsReport              bool                  `json:"is_report" bson:"is_report" gorm:"-"`
+	IsAccountReport       bool                  `json:"is_account_report" bson:"is_account_report" gorm:"-"`
+	IsCashflowReport      bool                  `json:"is_cashflow_report" bson:"is_cashflow_report" gorm:"-"`
+	IsPDF                 bool                  `json:"is_pdf" bson:"is_pdf" gorm:"-"`
+	Type                  AccountType           `json:"type" bson:"type"`
+	Category              string                `json:"category" bson:"category"`
+	CashflowGroup         string                `json:"cashflow_group" bson:"cashflow_group"`
+	CashflowSubGroup      string                `json:"cashflow_subgroup" bson:"cashflow_group"`
+	IsTax                 bool                  `json:"is_tax" bson:"is_tax" gorm:"default:false"`
+	TypeLabel             string                `gorm:"-" json:"type_label"`
+	CashflowGroupLabel    string                `gorm:"-" json:"cashflow_group_label"`
+	CashflowSubGroupLabel string                `gorm:"-" json:"cashflow_subgroup_label" `
+	CompanyID             *string               `json:"company_id"`
+	Company               *company.CompanyModel `gorm:"foreignKey:CompanyID"`
+	TransactionCount      int64                 `gorm:"-"`
+	Balance               float64               `gorm:"-"`
+	BalanceBefore         float64               `gorm:"-"`
+	HasOpeningBalance     bool                  `gorm:"-"`
 	// Transactions          []Transaction `gorm:"-"`
 }
 
