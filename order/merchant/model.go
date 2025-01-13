436541cb99889edc86b32333fba5f5ac460435b0
ametsuramet <ametsuramet@gmail.com>
2025-01-13 23:17:33 +0700

    add indonesia reg

--- order/merchant/model.go
+++ order/merchant/model.go
@@ -9,13 +9,17 @@ import (
 
 type MerchantModel struct {
 	shared.BaseModel
-	Name      string                `json:"name" gorm:"not null"`
-	Address   string                `json:"address" gorm:"not null"`
-	Phone     string                `json:"phone" gorm:"not null"`
-	Latitude  float64               `json:"latitude" gorm:"type:decimal(10,8);not null"`
-	Longitude float64               `json:"longitude" gorm:"type:decimal(11,8);not null"`
-	CompanyID *string               `json:"company_id,omitempty" gorm:"index"`
-	Company   *company.CompanyModel `json:"company,omitempty" gorm:"foreignKey:CompanyID"`
+	Name       string                `json:"name" gorm:"not null"`
+	Address    string                `json:"address" gorm:"not null"`
+	Phone      string                `json:"phone" gorm:"not null"`
+	Latitude   float64               `json:"latitude" gorm:"type:decimal(10,8);not null"`
+	Longitude  float64               `json:"longitude" gorm:"type:decimal(11,8);not null"`
+	CompanyID  *string               `json:"company_id,omitempty" gorm:"index"`
+	Company    *company.CompanyModel `json:"company,omitempty" gorm:"foreignKey:CompanyID"`
+	ProvinceID *string               `json:"province_id,omitempty" gorm:"type:char(2);index"`
+	RegencyID  *string               `json:"regency_id,omitempty" gorm:"type:char(4);index"`
+	DistrictID *string               `json:"district_id,omitempty" gorm:"type:char(6);index"`
+	VillageID  *string               `json:"village_id,omitempty" gorm:"type:char(10);index"`
 }
 
 func (m *MerchantModel) TableName() string {
--- shared/indonesia_regional/model.go
+++ shared/indonesia_regional/model.go
@@ -1,9 +1,9 @@
 package indonesia_regional
 
 type RegProvince struct {
-	ID        string
-	Name      string       `gorm:"type:varchar(100);not null"`
-	Regencies []RegRegency `gorm:"foreignKey:ProvinceID;references:ID"`
+	ID        string       `gorm:"primary_key" json:"id"`
+	Name      string       `gorm:"type:varchar(100);not null" json:"name"`
+	Regencies []RegRegency `gorm:"foreignKey:ProvinceID;references:ID" json:"regencies"`
 }
 
 func (RegProvince) TableName() string {
@@ -11,10 +11,10 @@ func (RegProvince) TableName() string {
 }
 
 type RegRegency struct {
-	ID         string
-	ProvinceID string        `gorm:"type:varchar(36);not null;index"`
-	Name       string        `gorm:"type:varchar(100);not null"`
-	Districts  []RegDistrict `gorm:"foreignKey:RegencyID;references:ID"`
+	ID         string        `gorm:"primary_key" json:"id"`
+	ProvinceID string        `gorm:"type:char(2);not null;index" json:"province_id"`
+	Name       string        `gorm:"type:varchar(100);not null" json:"name"`
+	Districts  []RegDistrict `gorm:"foreignKey:RegencyID;references:ID" json:"districts"`
 }
 
 func (RegRegency) TableName() string {
@@ -22,10 +22,10 @@ func (RegRegency) TableName() string {
 }
 
 type RegDistrict struct {
-	ID        string       `gorm:"type:varchar(36);not null;index"`
-	RegencyID string       `gorm:"type:varchar(36);not null;index"`
-	Name      string       `gorm:"type:varchar(100);not null"`
-	Villages  []RegVillage `gorm:"foreignKey:DistrictID;references:ID"`
+	ID        string       `gorm:"primary_key" json:"id"`
+	RegencyID string       `gorm:"type:varchar(4);not null;index" json:"regency_id"`
+	Name      string       `gorm:"type:varchar(100);not null" json:"name"`
+	Villages  []RegVillage `gorm:"foreignKey:DistrictID;references:ID" json:"villages"`
 }
 
 func (RegDistrict) TableName() string {
@@ -33,9 +33,9 @@ func (RegDistrict) TableName() string {
 }
 
 type RegVillage struct {
-	ID         string `gorm:"type:varchar(36);not null;index"`
-	DistrictID string `gorm:"type:varchar(36);not null;index"`
-	Name       string `gorm:"type:varchar(100);not null"`
+	ID         string `gorm:"primary_key" json:"id"`
+	DistrictID string `gorm:"type:varchar(6);not null;index" json:"district_id"`
+	Name       string `gorm:"type:varchar(100);not null" json:"name"`
 }
 
 func (RegVillage) TableName() string {
