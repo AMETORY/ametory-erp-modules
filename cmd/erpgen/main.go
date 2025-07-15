8970fb8abafd348fa3cdc6bf64d244e934d10b85
ametsuramet <ametsuramet@gmail.com>
2025-07-15 21:15:27 +0700

    release v1.0.44

--- cmd/erpgen/main.go
+++ cmd/erpgen/main.go
@@ -17,7 +17,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var version = "1.0.43"
+var version = "1.0.44"
 
 var logo = `
 MMMMMMMMMMMMMMMMWXOxoc;,............';coxOXWMMMMMMMMMMMMMMMM
--- internal/generatorlib/generator.go
+++ internal/generatorlib/generator.go
@@ -16,7 +16,7 @@ import (
 
 //go:embed templates/*
 var templateFS embed.FS
-var version = "1.0.43"
+var version = "1.0.44"
 
 func loadTemplate(name string) (*template.Template, error) {
 	content, err := fs.ReadFile(templateFS, "templates/"+name)
